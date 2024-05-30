9c03dad07c030429079dd42e734c00530f710fad
KevinRansom <codecutter@hotmail.com>
2024-05-30 01:17:55 -0700

    tests

--- tests/FSharp.Compiler.ComponentTests/ErrorMessages/Directives.fs
+++ tests/FSharp.Compiler.ComponentTests/ErrorMessages/Directives.fs
@@ -219,45 +219,6 @@ module DoBinding =
         |> shouldSucceed
 
 
-    [<Fact>]
-    let ``Time Errors F# 8`` () =
-
-        Fsx """
-#time ono;;
-#time 0n;;
-#time of;;
-printfn "Hello, World"
-        """
-        |> withLangVersion80
-        |> asExe
-        |> compile
-        |> shouldFail
-        |> withDiagnostics [
-            (Error 10, Line 3, Col 7, Line 3, Col 9, "Unexpected integer literal in implementation file")
-            (Error 3350, Line 2, Col 7, Line 2, Col 10, "Feature '# directives with non-quoted string arguments' is not available in F# 8.0. Please use language version 'PREVIEW' or greater.")
-            (Error 235, Line 2, Col 1, Line 2, Col 10, """Invalid directive. Expected '#time', '#time "on"' or '#time "off"'.""")
-            ]
-
-
-    [<Fact>]
-    let ``Time Errors F# 9`` () =
-
-        Fsx """
-#time ono;;
-#time 0n;;
-#time of;;
-printfn "Hello, World"
-        """
-        |> asExe
-        |> withLangVersionPreview
-        |> compile
-        |> shouldFail
-        |> withDiagnostics [
-            (Error 10, Line 3, Col 7, Line 3, Col 9, "Unexpected integer literal in implementation file")
-            (Error 235, Line 2, Col 1, Line 2, Col 10, """Invalid directive. Expected '#time', '#time "on"' or '#time "off"'.""")
-            ]
-
-
     [<InlineData("8.0")>]
     [<InlineData("preview")>]
     [<Theory>]
@@ -266,9 +227,16 @@ printfn "Hello, World"
         Fsx """
 #time on;;
 #time off;;
+#time blah;;
+#time Ident;;
+#time Long.Ident;;
+#time 123;;
+#time on off;;
 #time;;
 #time "on";;
 #time "off";;
+#time "blah";;
+#time "on" "off";;
 
 printfn "Hello, World"
         """
@@ -277,6 +245,25 @@ printfn "Hello, World"
         |> compile
         |> shouldFail
         |> withDiagnostics [
+            if langversion = "8.0" then
+                (Error 3350, Line 2, Col 7, Line 2, Col 9, "Feature '# directives with non-quoted string arguments' is not available in F# 8.0. Please use language version 'PREVIEW' or greater.")
+                (Error 3350, Line 3, Col 7, Line 3, Col 10, "Feature '# directives with non-quoted string arguments' is not available in F# 8.0. Please use language version 'PREVIEW' or greater.")
+                (Error 3350, Line 4, Col 7, Line 4, Col 11, "Feature '# directives with non-quoted string arguments' is not available in F# 8.0. Please use language version 'PREVIEW' or greater.")
+                (Error 3350, Line 5, Col 7, Line 5, Col 12, "Feature '# directives with non-quoted string arguments' is not available in F# 8.0. Please use language version 'PREVIEW' or greater.")
+                (Error 3350, Line 6, Col 7, Line 6, Col 17, "Feature '# directives with non-quoted string arguments' is not available in F# 8.0. Please use language version 'PREVIEW' or greater.")
+                (Error 3350, Line 7, Col 7, Line 7, Col 10, "Feature '# directives with non-quoted string arguments' is not available in F# 8.0. Please use language version 'PREVIEW' or greater.")
+                (Error 3350, Line 8, Col 7, Line 8, Col 9, "Feature '# directives with non-quoted string arguments' is not available in F# 8.0. Please use language version 'PREVIEW' or greater.")
+                (Error 3350, Line 8, Col 10, Line 8, Col 13, "Feature '# directives with non-quoted string arguments' is not available in F# 8.0. Please use language version 'PREVIEW' or greater.")
+                (Error 235, Line 12, Col 1, Line 12, Col 13, """Invalid directive. Expected '#time', '#time "on"' or '#time "off"'.""")
+                (Error 235, Line 13, Col 1, Line 13, Col 17, """Invalid directive. Expected '#time', '#time "on"' or '#time "off"'.""")
+            else
+                (Error 235, Line 4, Col 1, Line 4, Col 11, """Invalid directive. Expected '#time', '#time "on"' or '#time "off"'.""")
+                (Error 235, Line 5, Col 1, Line 5, Col 12, """Invalid directive. Expected '#time', '#time "on"' or '#time "off"'.""")
+                (Error 235, Line 6, Col 1, Line 6, Col 17, """Invalid directive. Expected '#time', '#time "on"' or '#time "off"'.""")
+                (Error 235, Line 7, Col 1, Line 7, Col 10, """Invalid directive. Expected '#time', '#time "on"' or '#time "off"'.""")
+                (Error 235, Line 8, Col 1, Line 8, Col 13, """Invalid directive. Expected '#time', '#time "on"' or '#time "off"'.""")
+                (Error 235, Line 12, Col 1, Line 12, Col 13, """Invalid directive. Expected '#time', '#time "on"' or '#time "off"'.""")
+                (Error 235, Line 13, Col 1, Line 13, Col 17, """Invalid directive. Expected '#time', '#time "on"' or '#time "off"'.""")
             ]
 
 
@@ -289,10 +276,15 @@ printfn "Hello, World"
 #time on;;
 #time off;;
 #time blah;;
+#time Ident;;
+#time Long.Ident;;
+#time 123;;
+#time on off;;
 #time;;
 #time "on";;
 #time "off";;
 #time "blah";;
+#time "on" "off";;
 
 printfn "Hello, World"
         """
@@ -301,6 +293,25 @@ printfn "Hello, World"
         |> compile
         |> shouldFail
         |> withDiagnostics [
+            if langversion = "8.0" then
+                (Error 3350, Line 2, Col 7, Line 2, Col 9, "Feature '# directives with non-quoted string arguments' is not available in F# 8.0. Please use language version 'PREVIEW' or greater.")
+                (Error 3350, Line 3, Col 7, Line 3, Col 10, "Feature '# directives with non-quoted string arguments' is not available in F# 8.0. Please use language version 'PREVIEW' or greater.")
+                (Error 3350, Line 4, Col 7, Line 4, Col 11, "Feature '# directives with non-quoted string arguments' is not available in F# 8.0. Please use language version 'PREVIEW' or greater.")
+                (Error 3350, Line 5, Col 7, Line 5, Col 12, "Feature '# directives with non-quoted string arguments' is not available in F# 8.0. Please use language version 'PREVIEW' or greater.")
+                (Error 3350, Line 6, Col 7, Line 6, Col 17, "Feature '# directives with non-quoted string arguments' is not available in F# 8.0. Please use language version 'PREVIEW' or greater.")
+                (Error 3350, Line 7, Col 7, Line 7, Col 10, "Feature '# directives with non-quoted string arguments' is not available in F# 8.0. Please use language version 'PREVIEW' or greater.")
+                (Error 3350, Line 8, Col 7, Line 8, Col 9, "Feature '# directives with non-quoted string arguments' is not available in F# 8.0. Please use language version 'PREVIEW' or greater.")
+                (Error 3350, Line 8, Col 10, Line 8, Col 13, "Feature '# directives with non-quoted string arguments' is not available in F# 8.0. Please use language version 'PREVIEW' or greater.")
+                (Error 235, Line 12, Col 1, Line 12, Col 13, """Invalid directive. Expected '#time', '#time "on"' or '#time "off"'.""")
+                (Error 235, Line 13, Col 1, Line 13, Col 17, """Invalid directive. Expected '#time', '#time "on"' or '#time "off"'.""")
+            else
+                (Error 235, Line 4, Col 1, Line 4, Col 11, """Invalid directive. Expected '#time', '#time "on"' or '#time "off"'.""")
+                (Error 235, Line 5, Col 1, Line 5, Col 12, """Invalid directive. Expected '#time', '#time "on"' or '#time "off"'.""")
+                (Error 235, Line 6, Col 1, Line 6, Col 17, """Invalid directive. Expected '#time', '#time "on"' or '#time "off"'.""")
+                (Error 235, Line 7, Col 1, Line 7, Col 10, """Invalid directive. Expected '#time', '#time "on"' or '#time "off"'.""")
+                (Error 235, Line 8, Col 1, Line 8, Col 13, """Invalid directive. Expected '#time', '#time "on"' or '#time "off"'.""")
+                (Error 235, Line 12, Col 1, Line 12, Col 13, """Invalid directive. Expected '#time', '#time "on"' or '#time "off"'.""")
+                (Error 235, Line 13, Col 1, Line 13, Col 17, """Invalid directive. Expected '#time', '#time "on"' or '#time "off"'.""")
             ]
 
 
