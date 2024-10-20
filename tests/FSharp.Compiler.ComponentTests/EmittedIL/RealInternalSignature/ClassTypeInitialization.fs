99f44e11bcbb3b9a4b8e6b76da510d8aafbb3357
Kevin Ransom <kevinr@microsoft.com>
2024-10-19 17:38:35 -0700

    temp

--- tests/FSharp.Compiler.ComponentTests/EmittedIL/RealInternalSignature/ClassTypeInitialization.fs
+++ tests/FSharp.Compiler.ComponentTests/EmittedIL/RealInternalSignature/ClassTypeInitialization.fs
@@ -1070,61 +1070,8 @@ type internal AgedLookup<'Token, 'Key, 'Value when 'Value: not struct>(keepStron
         FSharp """
 namespace Equality
 
-type SmallNonGenericTuple = SmallNonGenericTuple of int * string
-
-type SmallGenericTuple<'a> = SmallGenericTuple of int * 'a
-
-type BigNonGenericTuple = BigNonGenericTuple of int * string * byte * int * string * byte
-
 type BigGenericTuple<'a> = BigGenericTuple of int * 'a * byte * int * 'a * byte
 
-[<Struct>]
-type SmallNonGenericTupleStruct = SmallNonGenericTupleStruct of int * string
-
-[<Struct>]
-type SmallGenericTupleStruct<'a> = SmallGenericTupleStruct of int * 'a
-
-[<Struct>]
-type BigNonGenericTupleStruct = BigNonGenericTupleStruct of int * string * byte * int * string * byte
-
-[<Struct>]
-type BigGenericTupleStruct<'a> = BigGenericTupleStruct of int * 'a * byte * int * 'a * byte
-
-type ReferenceTuples() =
-
-    let numbers = Array.init 1000 id
-
-    member _.SmallNonGenericTuple() =
-        numbers
-        |> Array.countBy (fun n -> SmallNonGenericTuple(n, string n))
-
-    member _.SmallGenericTuple() =
-        numbers
-        |> Array.countBy (fun n -> SmallGenericTuple(n, string n))
-
-    member _.BigNonGenericTuple() =
-        numbers
-        |> Array.countBy (fun n -> BigNonGenericTuple(n, string n, byte n, n, string n, byte n))
-
-    member _.BigGenericTuple() =
-        numbers
-        |> Array.countBy (fun n -> BigGenericTuple(n, string n, byte n, n, string n, byte n))
-
-    member _.SmallNonGenericTupleStruct() =
-        numbers
-        |> Array.countBy (fun n -> SmallNonGenericTupleStruct(n, string n))
-
-    member _.SmallGenericTupleStruct() =
-        numbers
-        |> Array.countBy (fun n -> SmallGenericTupleStruct(n, string n))
-
-    member _.BigNonGenericTupleStruct() =
-        numbers
-        |> Array.countBy (fun n -> BigNonGenericTupleStruct(n, string n, byte n, n, string n, byte n))
-
-    member _.BigGenericTupleStruct() =
-        numbers
-        |> Array.countBy (fun n -> BigGenericTupleStruct(n, string n, byte n, n, string n, byte n))
     """
         |> asLibrary
         |> withRealInternalSignature realSig
